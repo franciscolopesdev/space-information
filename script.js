bc25afb1332ed8554329d92032c05710bc1ead2f
Dev Lopes <140910995+franciscolopesdev@users.noreply.github.com>
2024-11-19 20:10:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Inicializando o AOS (Animate on Scroll)
+AOS.init({
+    offset: 120, // Distância para ativar a animação
+    duration: 1000, // Tempo da animação
+    easing: 'ease-in-out', // Efeito de transição
+});
